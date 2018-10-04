9e11310c354abff4418db912d3ba143d40632ac3
becauseofyou <593572013@qq.com>
2018-10-05 07:47:33 +0800

    updateE removeE

--- codeforces/1060-c++14/E/E.cc
+++ codeforces/1060-c++14/E/E.cc
@@ -7,38 +7,30 @@ vector <int> e[N];
 
 long long ret;
 int n;
-int dp[N][2];
-int son[N];
-long long oddPair;
-long long sum;
+int son[N], cnt[2];
 
-void dfs(int u, int f) {
-	dp[u][0] = 1;
-    dp[u][1] = 0;
+void dfs(int u, int f, int d) {
+    cnt[d]++;
     son[u] = 1;
-	for (int v: e[u]) {
-		if (v != f) {
-			dfs(v, u);
+    for (int v: e[u]) {
+        if (v != f) {
+            dfs(v, u, d ^ 1);
             son[u] += son[v];
-            oddPair += 1LL * dp[u][0] * dp[v][0];
-            oddPair += 1LL * dp[u][1] * dp[v][1];
-            dp[u][0] += dp[v][1];
-            dp[u][1] += dp[v][0];
-		}
-	}
+        }
+    }
     if (f) {
-        sum += 1LL * son[u] * (n - son[u]);
+        ret += 1LL * son[u] * (n - son[u]);
     }
 }
 int main() { 
-	scanf("%d", &n);
-	int a, b;
-	for (int i = 1; i < n; i++) {
-		scanf("%d%d", &a, &b);
-		e[a].push_back(b);
-		e[b].push_back(a);
-	}
-	dfs(1, 0);
-    cout << (sum+oddPair) / 2 << endl;
-	return 0;
+    scanf("%d", &n);
+    int a, b;
+    for (int i = 1; i < n; i++) {
+        scanf("%d%d", &a, &b);
+        e[a].push_back(b);
+        e[b].push_back(a);
+    }
+    dfs(1, 0, 0);
+    cout << (ret + 1LL * cnt[0] * cnt[1]) / 2 << endl;
+    return 0;
 }
