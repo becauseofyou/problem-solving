86fe7614ec40e867ef5cd23459484d4c1555b7b3
becauseofyou <593572013@qq.com>
2018-10-17 21:19:39 +0800

    update-testsh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,30 +3,35 @@
 INPUT_NAME=input
 OUTPUT_NAME=output
 MY_NAME=my_output
+cppfile=B.cpp
 
-if ! g++-6 -g  -Wall B.cpp -std=c++11; then
+if ! g++ -g  -Wall $cppfile -std=c++11; then
     exit
 fi
 
 #删除所有用户输出而且不显示错误信息 
 rm -R $MY_NAME* 2>/dev/null
 
-
+all=0
+passed=0
 for test_file in $INPUT_NAME*
 do
+    all=$(($all+1))
     #去除INPUT_NAME前缀
     i=${#INPUT_NAME} 
     test_case=${test_file:$i}
     if ! ./a.out < $INPUT_NAME$test_case > $MY_NAME$test_case; then
-        echo [1m[31mSample test \#$test_case: Runtime Error[0m 
+        echo -e "\033[31mSample test \#$test_case: Runtime Error \033[0m"
         echo ========================================
         echo Sample Input \#$test_case
         cat $INPUT_NAME$test_case
+        all
     else
         if diff --brief $MY_NAME$test_case $OUTPUT_NAME$test_case; then
-            echo [33m[34mSample test \#$test_case: Accepted[0m 
+            echo -e "\033[42;30mSample test #$test_case: Passed \033[0m"
+			passed=$(($passed+1))
         else
-            echo [1m[31mSample test \#$test_case: Wrong Answer[0m 
+            echo -e "\033[41;30mSample test #$test_case: 你红了 \033[0m"
         fi
         echo ========================================
         echo Sample Input \#$test_case
@@ -38,5 +43,11 @@ do
         echo My Output \#$test_case
         cat $MY_NAME$test_case
         echo ========================================
+        echo -e
     fi
 done
+if (($all == $passed)); then
+	echo -e "\033[42;30m Successfully Passed All the $all tests \033[0m"
+else
+	echo -e  "\033[43;34m 一共有$all组数据，你通过了$passed组，红了$(($all-$passed))组 \033[0m" 
+fi
