7912365a7f036002723360ea57566e849c70729b
becauseofyou <593572013@qq.com>
2018-10-29 00:56:38 +0800

    template

---
+++ reference/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define pii pair<int,int>
+#define X first
+#define Y second
+
+template <typename T> void checkmin(T &a, T b) { if (a > b) { a = b; } }
+template <typename T> void checkmax(T &a, T b) { if (a < b) { a = b; } }
+
+int main() {
+    return 0;
+}
