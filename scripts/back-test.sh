de90839d0bfc3f2fb21d363a6e34e3c303528e64
吴一岐 <wuyiqi@wuyiqideMacBook-Pro.local>
2018-12-09 21:39:32 +0800

    scripts

--- scripts/back-test.sh
+++ scripts/back-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # pip install when-changed, 监控文件变动并且文件修改之后自动执行 pytest 单测，方便我们边修改边跑测试
-when-changed -v -r -1 -s ./    "./test.sh $1" 
+when-changed -v -r -1 -s $1    "./mac-test.sh $1" 
 # 用法： ./back-test.sh X.cpp
 
---
+++ scripts/mac-test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+INPUT_NAME=input
+OUTPUT_NAME=output
+MY_NAME=my_output
+cppfile=B.cpp
+
+if ! g++-6 -g  -Wall $1 -std=c++11; then
+    exit
+fi
+
+#删除所有用户输出而且不显示错误信息 
+rm -R $MY_NAME* 2>/dev/null
+
+all=0
+passed=0
+for test_file in $INPUT_NAME*
+do
+    all=$(($all+1))
+    #去除INPUT_NAME前缀
+    i=${#INPUT_NAME} 
+    test_case=${test_file:$i}
+    if ! ./a.out < $INPUT_NAME$test_case > $MY_NAME$test_case; then
+        echo -e "\033[31mSample test \#$test_case: Runtime Error \033[0m"
+        echo ========================================
+        echo Sample Input \#$test_case
+        cat $INPUT_NAME$test_case
+        all
+    else
+        if diff --brief $MY_NAME$test_case $OUTPUT_NAME$test_case; then
+            echo -e "\033[42;30mSample test #$test_case: Passed \033[0m"
+			passed=$(($passed+1))
+        else
+            echo -e "\033[41;30mSample test #$test_case: 你红了 \033[0m"
+            echo ========================================
+            echo Sample Input \#$test_case
+                cat $INPUT_NAME$test_case
+            echo ========================================
+            echo Sample Output \#$test_case
+                cat $OUTPUT_NAME$test_case
+            echo ========================================
+            echo My Output \#$test_case
+                cat $MY_NAME$test_case
+            echo ========================================
+            echo -e
+        fi
+    fi
+done
+if (($all == $passed)); then
+    echo -e "\033[42;30m Successfully Passed All the $all tests \033[0m"
+else
+    echo -e  "\033[43;34m 一共有$all组数据，你通过了$passed组，红了$(($all-$passed))组 \033[0m" 
+fi
+rm -rf a.out*
